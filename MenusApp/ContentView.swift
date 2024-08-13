3e4ad02e4ca9cd611825dc5a40fc1ce7db97bdc2
Steven barnett <steve@sabarnett.co.uk>
2024-08-13 13:15:03 +0100

    Initial Commit

---
+++ MenusApp/ContentView.swift
@@ -0,0 +1,29 @@
+//
+// -----------------------------------------
+// Original project: MenusApp
+// Original package: MenusApp
+// Created on: 13/08/2024 by: Steven Barnett
+// Web: http://www.sabarnett.co.uk
+// GitHub: https://www.github.com/sabarnett
+// -----------------------------------------
+// Copyright © 2024 Steven Barnett. All rights reserved.
+//
+    
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MenusApp/MenusAppApp.swift
@@ -0,0 +1,22 @@
+//
+// -----------------------------------------
+// Original project: MenusApp
+// Original package: MenusApp
+// Created on: 13/08/2024 by: Steven Barnett
+// Web: http://www.sabarnett.co.uk
+// GitHub: https://www.github.com/sabarnett
+// -----------------------------------------
+// Copyright © 2024 Steven Barnett. All rights reserved.
+//
+    
+
+import SwiftUI
+
+@main
+struct MenusAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
